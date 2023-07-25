f8da04b1c90012c288d6d2db480f3e2b47a60ebc
Artem <tillerman12@gmail.com>
2023-07-25 12:59:03 +0500

    1 TZ

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 
 @RestController
 public class FilmController {
