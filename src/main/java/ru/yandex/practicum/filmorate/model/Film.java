cee9b0d61d539f83fc901781083122ff4fb2bf24
Artem <tillerman12@gmail.com>
2023-07-26 16:37:31 +0500

    9 TZ

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -8,7 +8,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
-import java.util.Date;
 
 @Data
 public class Film {
